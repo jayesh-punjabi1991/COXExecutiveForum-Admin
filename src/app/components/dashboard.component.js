53a68d7a9b63b4236ad961316f3422d7ceda232f
Punjabi <400219826@ind.ge.ad>
2017-10-06 18:59:23 +0530

    Final Commit Before Demo

--- src/app/components/dashboard.component.js
+++ src/app/components/dashboard.component.js
@@ -14,7 +14,14 @@ var DashboardComponent = (function () {
         this.sponsors = JSON.parse(sessionStorage.getItem("Sponsors"));
         this.number = this.sponsors.registeredUsers.length;
         this.total = this.number * 60000;
+        for (this.i = 0; this.i < this.sponsors.registeredUsers.length; this.i++) {
+            this.temp = new Date(this.sponsors.registeredUsers[this.i].registrationDate);
+            this.sponsors.registeredUsers[this.i].registrationDate = this.temp.getMonth() + 1 + '/' + this.temp.getDate() + '/' + this.temp.getFullYear();
+        }
+        console.log(this.sponsors.registeredUsers);
     }
+    DashboardComponent.prototype.refresh = function () {
+    };
     return DashboardComponent;
 }());
 DashboardComponent = __decorate([
